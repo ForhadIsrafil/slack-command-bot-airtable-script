9dc817be2e4ebc973680d8948882044d0b2ee6b7
forhadisrafil <forhadtuhin5088@egmail.com>
2023-09-11 00:45:26 +0600

    up

--- slack_bot_2023/main.py
+++ slack_bot_2023/main.py
@@ -27,20 +27,39 @@ def partybot_report(ack, say, command):
     user_id = command['user_id']
     channel_id = command['channel_id']
 
-    # Filtering Datetime in Unix Timestamp start
+    # FILTERING DATETIME IN UNIX TIMESTAMP START
     today_date_time = datetime.datetime.now()
-    print("first day Date:", today_date_time.day - today_date_time.day + 1)
+    # print("first day Date:", today_date_time.day - today_date_time.day + 1)
     # latest_date_time
-    latest_date_time = time.mktime(today_date_time.timetuple())
-    print("latest UNIX timestamp:", latest_date_time)
+    latest_date_unix_time = time.mktime(today_date_time.timetuple())
+    print("latest UNIX timestamp:", latest_date_unix_time)
+
     # oldest_date_time
     oldest_date_time = datetime.datetime(year=today_date_time.year, month=today_date_time.month,
-                                         day=today_date_time.day - today_date_time.day + 1, hour=0, minute=1, second=1)
-    print("oldest UNIX timestamp:", time.mktime(oldest_date_time.timetuple()))
-    # Filtering Datetime in Unix Timestamp end
+                                         day=1, hour=0, minute=0, second=1)
+    oldest_date_unix_time = time.mktime(oldest_date_time.timetuple())
+    print("oldest UNIX timestamp:", oldest_date_unix_time)
+    # FILTERING DATETIME IN UNIX TIMESTAMP END
 
-    messages = app.client.conversations_history(channel=channel_id, oldest=oldest_date_time, limit=1000)
-    print(messages)
+    messages = app.client.conversations_history(channel=channel_id, latest=latest_date_unix_time,
+                                                oldest=oldest_date_unix_time, limit=700)
+    # print(messages)
+    # ------------------------------------------------------------------------------------------
+    parated_reactions_count_dict = {}
+    for react in messages:
+        try:
+            # print(react['reactions'])
+            if react['reactions'][0]['name'] not in separated_reactions_count_dict:
+                separated_reactions_count_dict[react['reactions'][0]['name']] = 1
+
+            if react['reactions'][0]['name'] in separated_reactions_count_dict:
+                separated_reactions_count_dict[react['reactions'][0]['name']] = separated_reactions_count_dict[
+                                                                                    react['reactions'][0]['name']] + 1
+        except Exception as e:
+            pass
+
+    print(separated_reactions_count_dict)
+    # ------------------------------------------------------------------------------------------
     say("hey")
 
 
--- slack_bot_2023/tojson.py
+++ slack_bot_2023/tojson.py
@@ -1,12 +1,32 @@
 import datetime
 import time
+import json
 
-today_date_time = datetime.datetime.now()
-print("first day Date:", today_date_time.day - today_date_time.day + 1)
+separated_reactions_count_dict = {}
+with open("json_data.json", 'r') as json_file:
+    data = json.loads(json_file.read())
+    messages = data['messages']
 
-latest_date_time = time.mktime(today_date_time.timetuple())
-print("latest UNIX timestamp:", latest_date_time)
+    for react in messages:
+        try:
+            # print(react['reactions'])
+            if react['reactions'][0]['name'] not in separated_reactions_count_dict:
+                separated_reactions_count_dict[react['reactions'][0]['name']] = 1
 
-oldest_date_time = datetime.datetime(year=today_date_time.year, month=today_date_time.month,
-                                     day=today_date_time.day - today_date_time.day + 1, hour=0, minute=0, second=0)
-print("oldest UNIX timestamp:", time.mktime(oldest_date_time.timetuple()))
+            if react['reactions'][0]['name'] in separated_reactions_count_dict:
+                separated_reactions_count_dict[react['reactions'][0]['name']] = separated_reactions_count_dict[
+                                                                                    react['reactions'][0]['name']] + 1
+
+        except Exception as e:
+            pass
+print(separated_reactions_count_dict)
+# today_date_time = datetime.datetime.today()
+# print("first day Date:", today_date_time.day - today_date_time.day + 1)
+#
+# latest_date_time = time.mktime(today_date_time.timetuple())
+# print("latest UNIX timestamp:", latest_date_time)
+#
+# oldest_date_time = datetime.datetime(year=today_date_time.year, month=today_date_time.month,
+#                                      day=today_date_time.day - today_date_time.day + 1, hour=0, minute=1, second=1)
+# oldest_date_unix_time = time.mktime(oldest_date_time.timetuple())
+# print("oldest UNIX timestamp:", oldest_date_unix_time)
